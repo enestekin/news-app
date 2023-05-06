84ddc79fd61627957301ab3e229a330485bf5bdf
Enes Tekin <enestekin@Enes-MacBook-Pro.local>
2023-05-06 21:04:51 +0300

    fetching news

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import type { AppProps } from "next/app";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Container } from "react-bootstrap";
+import styles from "@/styles/App.module.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container className={styles.pageContainer}>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,40 @@
+import NewsArticlesGrid from "@/components/NewsArticlesGrid";
+import { NewsArticle, NewsResponse } from "@/models/NewsArticles";
+import { GetServerSideProps } from "next";
 import { Inter } from "next/font/google";
+import { Alert } from "react-bootstrap";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  return <main className={inter.className}>Hello world</main>;
+interface BreakingNewsPageProps {
+  newsArticles: NewsArticle[];
+}
+
+export const getServerSideProps: GetServerSideProps<
+  BreakingNewsPageProps
+> = async () => {
+  const response = await fetch(
+    "https://newsapi.org/v2/top-headlines?country=us&apiKey=" +
+      process.env.NEWS_API_KEY
+  );
+  const newsResponse: NewsResponse = await response.json();
+  return {
+    props: { newsArticles: newsResponse.articles },
+  };
+};
+
+export default function BreakingNewsPage({
+  newsArticles,
+}: BreakingNewsPageProps) {
+  return (
+    <main className={inter.className}>
+      <h1>Breaking News</h1>
+      <Alert>
+        This page uses <strong>getServerSideProps</strong> to fetch data
+        server-side on every request. This allows search engines to crawl the
+        page content and <strong>improves SEO</strong>.
+      </Alert>
+      <NewsArticlesGrid articles={newsArticles} />
+    </main>
+  );
 }
